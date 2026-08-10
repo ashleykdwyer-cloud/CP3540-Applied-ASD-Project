414e34da9e51b324f9c60e7430855d91c9b343de
Ashley Dwyer <ashleykdwyer@gmail.com>
2026-08-10 14:10:09 -0230

    Small changes to supervisor dashboard and moved pages out of js folder

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 app.use("/api/notifications", notificationRoute);
 app.use("/api/tasks", taskRoute);
 app.use("/api/timelogs", timelogRoute);
